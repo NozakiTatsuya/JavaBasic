245e8a63e1cb75ba2a2c9452b1502155d8a2ada8
tatsuya.nozaki.likeit@gmail.com <socc>
2018-12-27 14:10:36 +0900

    14-2課題を提出します。よろしくお願いします。

---
+++ Practice/src/practice14/common/DBCourse.java
@@ -0,0 +1,27 @@
+package practice14.common;
+
+
+
+public DBCourse class Course {
+
+	public String getCourseName() {
+		return "「【Eラーニング】DB基礎」";
+	}
+	public String[] getCourseUnit() {
+		String[] getCourseUnit= {"DB基礎","SQL基礎","正規化","SQL応用"};
+		return getCourseUnit;
+
+}
+}
+
+	/*
+	 * ★ common.Courseを実装した、DBCourseクラスを作成してください
+	 *
+	 * コース名称
+	 * 		「【Eラーニング】DB基礎」
+	 * 単元
+	 * 		DB基礎
+	 * 		SQL基礎
+	 * 		正規化
+	 * 		SQL応用
+	 */
